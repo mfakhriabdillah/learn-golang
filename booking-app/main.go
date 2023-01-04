0083c16ce8c4b71939092d7e7a22377b64450091
Muhammad Fakhri Abdillah <fakhriabdillah37@gmail.com>
2023-01-04 11:15:04 +0700

    Day 1 - Add Go Webserver

---
+++ booking-app/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var eventName = "Go Events"
+	const eventTickets = 70
+
+	fmt.Println("Welcome to ", eventName)
+	fmt.Println("This is your ticket")
+
+}
